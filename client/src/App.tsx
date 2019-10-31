1583c6313d0070f627411b8c1a341b77df16b5b1
Trevor Keith <keithtw@gmatc.matc.edu>
2019-10-30 22:13:44 -0500

    React setup

---
+++ client/src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          Forums
+
+        </header>
+
+      </div>
+
+    );
+
+  }
+
+}
+
+export default App;
