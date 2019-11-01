83fb1a7fd01e46208860f5bf0b36a6f591e01fb2
Trevor Keith <keithtw@gmatc.matc.edu>
2019-10-31 19:22:29 -0500

    Connected to API

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,29 @@
 import React from 'react';
+import axios from 'axios';
 import './App.css';
 
 class App extends React.Component {
+  state = {
+    users: []
+
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:5000/api/values')
+      .then((response) => {
+        this.setState({
+          users: response.data
+
+        })
+
+      })
+      .catch((error) => {
+        console.error(`Error fetching data: ${error}`)
+
+      })
+
+  }
+
   render() {
     return (
       <div className="App">
@@ -10,10 +32,11 @@ class App extends React.Component {
 
         </header>
 
+        {this.state.users.map((user: any) => <div key={user}>{user}</div>)}
+
       </div>
 
     );
-
   }
 
 }
